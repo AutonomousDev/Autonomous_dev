829c7a231a9bb79de7c445db2f4eac6782d18de3
Cameron Bowers <cameronbowers0@gmail.com>
2021-11-25 22:39:12 -0800

    Partial integration of login/logout

--- autonomous_dev/blog/urls.py
+++ autonomous_dev/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('about', views.about, name='blog-about'),
+    path('about/', views.about, name='blog-about'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
 ]
