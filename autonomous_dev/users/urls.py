d0e133c53bc6df371b0cce4b6827f86d19b1741c
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-12 10:40:22 -0800

    Register user basic setup.

--- autonomous_dev/users/urls.py
+++ autonomous_dev/users/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('', include('users.urls')),
+    path('register', views.register, name='register'),
 ]
