f22bf04889538f8abdf9a4eec15d21624e09d57b
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-12 14:45:06 -0800

    Login/ logout works

--- autonomous_dev/users/urls.py
+++ autonomous_dev/users/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path
 from . import views as user_views
 from django.contrib.auth import views as auth_views
 
@@ -21,5 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
 ]
