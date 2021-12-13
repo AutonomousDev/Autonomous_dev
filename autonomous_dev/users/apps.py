52bf7c9dc5a9495d63d352e8a01a341149ec51ec
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-12 17:12:45 -0800

    Profiles auto create with new users.

--- autonomous_dev/users/apps.py
+++ autonomous_dev/users/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        """Used to auto create profiles for new users with signals"""
+        import users.signals
+
