567a585f577584e5bf49ffc5d010073e671fc062
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-12 17:00:06 -0800

    Basic profiles working

--- autonomous_dev/autonomous_dev/urls.py
+++ autonomous_dev/autonomous_dev/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -22,3 +24,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- autonomous_dev/users/models.py
+++ autonomous_dev/users/models.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(null=True, blank=True, upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
