e7123c0aff3df01946998e373b8998f366fd88c3
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-13 16:09:17 -0800

    Profile updates works

--- autonomous_dev/users/forms.py
+++ autonomous_dev/users/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
+    """This form is used for registering new users"""
     email = forms.EmailField()
     first_name = forms.CharField()
     last_name = forms.CharField()
@@ -11,3 +13,21 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    """This Form is used for updating the users data"""
+    email = forms.EmailField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    """This form is used for updating the users profile"""
+    class Meta:
+        model = Profile
+        fields = ['image']
