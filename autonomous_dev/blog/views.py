90fbd92cc785881bd28ad6981d5eb14f327ff5c9
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-13 17:36:25 -0800

    CRUD operations added to posts + some style changes to give the end user links to the new CRUD functions.

--- autonomous_dev/blog/views.py
+++ autonomous_dev/blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
+''' #Phased out
 def home(request):
     """This view is the home page. The blog will be shown here."""
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+'''
 
 
 def about(request):
@@ -16,3 +20,53 @@ def about(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/about.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        """Set author before validating the form"""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        """Set author before validating the form"""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
