cad64073f3bb5cc2ec62426e4ebba32f7d1809a4
Cameron Bowers <cameronbowers0@gmail.com>
2021-12-18 17:21:04 -0800

    AWS S3 support added to host files.

--- autonomous_dev/settings.py
+++ autonomous_dev/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap5',
+    'storages',
 ]
 # Crispy forms config for bootstrap5
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -160,3 +161,12 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 django_heroku.settings(locals())
+
+
+# AWS settings
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
