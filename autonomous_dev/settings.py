5ae4db98721cd427fd07ef51859deb479a7e3412
Cameron Bowers <cameronbowers0@gmail.com>
2023-02-25 11:55:33 -0800

    Changed some requirements. Updated packages. Updated settings.

--- autonomous_dev/settings.py
+++ autonomous_dev/settings.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-from dotenv import load_dotenv
-
-load_dotenv()
+import dj_database_url
+from decouple import config
+from django.conf.global_settings import DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,24 +23,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = config('SECRET_KEY')
 
 
 # Deployment security stuff
-SECURE_BROWSER_XSS_FILTER = not bool(os.environ.get('DEBUG_MODE'))
-SECURE_CONTENT_TYPE_NOSNIFF = not bool(os.environ.get('DEBUG_MODE'))
-SECURE_SSL_REDIRECT = not bool(os.environ.get('DEBUG_MODE'))
+SECURE_BROWSER_XSS_FILTER = not config('DEBUG_MODE', default=False, cast=bool)
+SECURE_CONTENT_TYPE_NOSNIFF = not config('DEBUG_MODE', default=False, cast=bool)
+SECURE_SSL_REDIRECT = not config('DEBUG_MODE', default=False, cast=bool)
 SECURE_HSTS_SECONDS = 86400  # Equivalent to 1 day
-SECURE_HSTS_PRELOAD = not bool(os.environ.get('DEBUG_MODE'))
-SECURE_HSTS_INCLUDE_SUBDOMAINS = not bool(os.environ.get('DEBUG_MODE'))
-SESSION_COOKIE_SECURE = not bool(os.environ.get('DEBUG_MODE'))
+SECURE_HSTS_PRELOAD = not config('DEBUG_MODE', default=False, cast=bool)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = not config('DEBUG_MODE', default=False, cast=bool)
+SESSION_COOKIE_SECURE = not config('DEBUG_MODE', default=False, cast=bool)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_COOKIE_SECURE = not bool(os.environ.get('DEBUG_MODE'))
+CSRF_COOKIE_SECURE = not config('DEBUG_MODE', default=False, cast=bool)
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = bool(os.environ.get('DEBUG_MODE'))
+DEBUG = config('DEBUG_MODE', default=False, cast=bool)
 
 # Makes Error messages red
 from django.contrib.messages import constants as messages
@@ -50,7 +49,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'autonomous-dev.herokuapp.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
@@ -63,10 +62,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_bootstrap5',
-    'storages',  # used by AWS S3
-    'tinymce',  # used by Tinymce
+    'crispy_forms',                 # Used to apply bootstrap to forms
+    'crispy_bootstrap5',            # Used for Bootstrap 5 styles
+    'storages',                     # used by AWS S3
+    'tinymce',                      # used by Tinymce
 ]
 SITE_ID = 1
 
@@ -107,12 +106,8 @@ WSGI_APPLICATION = 'autonomous_dev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'),conn_max_age=600)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -169,15 +164,16 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
 
-django_heroku.settings(locals())
+# Is this unused?
+# django_heroku.settings(locals())
 
 # AWS settings
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
